efeb9a483fecb7e2c8c28f96e90e0a37d52ef46d
MoeZuneThway <143493207+MoeZuneThway@users.noreply.github.com>
2024-10-16 15:07:27 +0100

    update layout

--- Vue.js App/app.js
+++ Vue.js App/app.js
@@ -1,26 +1,17 @@
 const app = new Vue({
-    el:'#app',
-    data:{
-        sitename:'EduXplore',
+    el: '#app',
+    data: {
+        sitename: 'EduXplore',
         bannerimage: "Vue.js App/images/playing 1.jpg",
-        activity:{
-            id: 1,
-            title:"Football",
-            description:"Join our 3-month football training program for children!",
-            location:"Hendon, London",
-            price: 30.00,
-            image: "images/playing 1.jpg",
-            availableSpace: 10,
-            
-        },
+        activities: activities,
         states: {
             AL: 'Alabama',
             AK: 'Alaska',
             AR: 'Arizona',
             CA: 'California',
             NV: 'Nevada'
-          },
-          order: {
+        },
+        order: {
             firstName: '',
             lastName: '',
             address: '',
@@ -30,42 +21,51 @@ const app = new Vue({
             method: 'Home Address',
             business: 'Business Address',
             home: 'Home Address',
-            gift:'Send As A Gift',
+            gift: 'Send As A Gift',
             sendGift: 'Send As A Gift',
             dontSendGift: 'Do Not Send As A Gift'
-          },
-        cart:[],
+        },
+        cart: [],
         showactivity: true,
-        type : 'Home'
+        type: 'Home'
     },
-    computed:{
-        cartItemCount(){
+    computed: {
+        cartItemCount() {
             return this.cart.length;
         },
-        canAddtoCart(){
-            return this.activity.availableSpace > this.cartItemCount;
-        },
-        canCheckout(){
+        canCheckout() {
             return this.cartItemCount > 0;
-        },
-        spaceLeft() {
-            return this.activity.availableSpace - this.cartItemCount;
-           }
+        }
     },
-    methods:{
-        addToCart(id){
-                this.cart.push(id);  
+    methods: {
+        addToCart(activity) {
+            this.cart.push(activity.id);
+        },
+        canAddtoCart(activity) {
+            return activity.availableSpace > this.cartCount(activity.id);
+        },
+        cartCount(id) {
+            let count = 0;
+            for (let i = 0; i < this.cart.length; i++) {
+                if (this.cart[i] === id) {
+                    count++;
+                }
+            }
+            return count;
+        },
+        spaceLeft(activity) {
+            return activity.availableSpace - this.cartCount(activity.id);
         },
         showCheckout() {
-            this.showactivity = this.showactivity ? false : true;
+            this.showactivity = !this.showactivity;
             console.log(this.showactivity);
-            },
-            toActivity(){
-                this. type = "Activity"
-            },
-            toHome(){
-                this.type = "Home"
-            }
+        },
+        toActivity() {
+            this.type = "Activity";
+        },
+        toHome() {
+            this.type = "Home";
+        }
     }
-    
 });
+    
